896f28ba3205724003a4bac82c60f85127bff1c6
Cpt-Umi <umairkhan.2102@gmail.com>
2023-08-14 14:54:36 +0500

    Weather widget done.

--- weather-app/src/api.js
+++ weather-app/src/api.js
@@ -1,4 +1,6 @@
 export const url = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "da790c0ea90229521ae6dc1d95b0eb30";
 export const geoApiOptions = {
   method: "GET",
   headers: {
